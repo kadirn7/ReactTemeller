d83eff95366a7b08b01fc61064883c36464d0da6
Kadir <Kadir@DESKTOP-P6D4VNP>
2025-02-04 00:19:42 +0300

    useState Kullanımı

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -5,29 +5,36 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const[firstName,setFirstName] = useState("kadir")
+  const [names,setNames] = useState(["kadir","ali","ayşe"])
 
+  const handleChange=()=>
+  {
+    setFirstName("Mehmet")
+  }
+  const handleChange2=(e)=>
+  {
+    setFirstName(e.target.value)
+  }
+  const handleChange3=(e)=>
+  {
+    setNames([...names,e.target.value])
+  }
+  
+  const Sayac=()=>{
+    setCount(count+1)
+  }
+  const TersSayaç=()=>{
+    setCount(count-1)
+  }
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>{count}</h1>
+        <button onClick={Sayac}>+</button>
+        <button onClick={TersSayaç}>-</button>
+       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
