131d53012e5706c4a20b1bb4d8b7d7ab82e0e3ba
kadirn7 <kadirneidik398@gmail.com>
2025-02-14 22:14:59 +0300

    redux toolkit added project

---
+++ src/Components/Login.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+
+export const users =[
+    {
+      username:"ali",
+      password:"1"
+    },
+    {
+      username:"veli",
+      password:"2"
+    }
+  ]
+
+  
+function Login(){
+    return <div>
+        <h1>Login</h1>
+    </div>
+}
+
+export default Login;
