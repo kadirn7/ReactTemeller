448df09a0c5e79f56ec8fb8564435d53f664e660
kadirn7 <kadirneidik398@gmail.com>
2025-02-15 10:23:08 +0300

    useSelector,useDispatch,store,slice kullanımı

--- reduxToolKit/src/App.jsx
+++ reduxToolKit/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from '../redux/counter.Slice'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state) => state.counter.value)
+  console.log(count)
+  const dispatch = useDispatch()
 
   return (
     <>
       <div>
-        
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
       </div>
       
     </>
